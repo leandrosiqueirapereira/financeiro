df6c3631b7f895487526d7e22bafe143fd674270
Munif Gebara Junior <munifgebara@gmail.com>
2016-03-28 20:51:32 -0300

    Criando aplicação WEB

--- src/main/java/br/com/munif/financeiro/Programa.java
+++ src/main/java/br/com/munif/financeiro/Programa.java
@@ -1,27 +1,33 @@
 package br.com.munif.financeiro;
 
-import br.com.munif.financeiro.entidades.Caixa;
-import br.com.munif.financeiro.entidades.Pessoa;
-import br.com.munif.financeiro.entidades.Titulo;
-import br.com.munif.financeiro.negocio.SuperEntidadeService;
-import br.com.munif.financeiro.repositorio.SuperEntidadeRepositorio;
-import br.com.munif.financeiro.util.Persistencia;
-import java.util.List;
-import javax.persistence.EntityManager;
+import br.com.munif.financeiro.entidades.Saldo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 
 public class Programa {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException, IOException {
         System.out.println("Incio");
+        
+        String json="{\"codigo\":1459208252077000,\"quando\":1459208252077,\"valor\":0,\"caixa\":{\"codigo\":1459208252076000,\"nome\":\"Caixa central\"}}";
+                
+        ObjectMapper mapper = new ObjectMapper();
+        Saldo saldo=mapper.readValue(json, Saldo.class);
+        
+        System.out.println("--->"+saldo.getCaixa().getNome());
+        
 
-        SuperEntidadeService<Caixa> caixaService = new SuperEntidadeService<>(Caixa.class);
-        EntityManager entityManager = Persistencia.getInstancia().getEntityManager();
-        entityManager.getTransaction().begin();
-        Caixa caixa = new Caixa();
-        caixa.setNome("Novo caixa");
-        System.out.println(""
-                + caixaService.salvar(caixa));
-        entityManager.getTransaction().commit();
+
+
+//        SuperEntidadeService<Caixa> caixaService = new SuperEntidadeService<>(Caixa.class);
+//        EntityManager entityManager = Persistencia.getInstancia().getEntityManager();
+//        entityManager.getTransaction().begin();
+//        Caixa caixa = new Caixa();
+//        caixa.setNome("Novo caixa");
+//        System.out.println(""
+//                + caixaService.salvar(caixa));
+//        entityManager.getTransaction().commit();
         //   entityManager.close();
 //        Persistencia.getInstancia().encerra();
     }
