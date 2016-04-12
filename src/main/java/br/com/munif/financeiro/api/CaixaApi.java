1d105b2d13be55412e04f6721ded4d64996311d1
Munif Gebara Junior <munifgebara@gmail.com>
2016-04-11 21:44:59 -0300

    alteracoes

--- src/main/java/br/com/munif/financeiro/api/CaixaApi.java
+++ src/main/java/br/com/munif/financeiro/api/CaixaApi.java
@@ -1,11 +1,8 @@
 package br.com.munif.financeiro.api;
 
 import br.com.munif.financeiro.entidades.Caixa;
-import java.io.IOException;
-import javax.servlet.ServletException;
+import br.com.munif.financeiro.negocio.SuperEntidadeService;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "CaixaApi", urlPatterns = {"/api/caixa/*"})
 public class CaixaApi extends SuperEntidadeApi<Caixa> {
