b5cb2ba9d1a8b4c2e389f99536702058272d90f0
Munif Gebara Junior <munifgebara@gmail.com>
2016-05-30 20:33:53 -0300

    Conflitos resolvidos

--- src/main/java/br/com/munif/financeiro/Programa.java
+++ src/main/java/br/com/munif/financeiro/Programa.java
@@ -1,95 +1,12 @@
 package br.com.munif.financeiro;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import br.com.munif.financeiro.entidades.Caixa;
-import br.com.munif.financeiro.entidades.MovimentoCaixa;
-import br.com.munif.financeiro.entidades.Saldo;
-import br.com.munif.financeiro.negocio.SuperEntidadeService;
-import br.com.munif.financeiro.util.Persistencia;
-import br.com.munif.financeiro.util.SuperEntidade;
-=======
-import br.com.munif.financeiro.entidades.Juridica;
-import br.com.munif.financeiro.entidades.Saldo;
-import br.com.munif.financeiro.negocio.SuperEntidadeService;
->>>>>>> 3d1f1b6ab087d1ebd6337b45af6813333007803e
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.Date;
-import javax.persistence.EntityManager;
-=======
-import br.com.munif.financeiro.entidades.Pagamento;
-import br.com.munif.financeiro.entidades.Saldo;
-import br.com.munif.financeiro.negocio.SuperEntidadeService;
-import br.com.munif.financeiro.util.Persistencia;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
->>>>>>> 358b6331c011dc952dae0c6b2d18592ccb4b571e
 
 public class Programa {
 
     public static void main(String[] args) throws JsonProcessingException, IOException {
         System.out.println("Incio");
-//        
-//        String json="{\"codigo\":1459208252077000,\"quando\":1459208252077,\"valor\":0,\"caixa\":{\"codigo\":1459208252076000,\"nome\":\"Caixa central\"}}";
-//                
-//        ObjectMapper mapper = new ObjectMapper();
-//        Saldo saldo=mapper.readValue(json, Saldo.class);
-//        
-//        System.out.println("--->"+saldo.getCaixa().getNome());
-        SuperEntidadeService<Pagamento>  entidadeService =new SuperEntidadeService<>(Pagamento.class);
-        EntityManager entityManager  =Persistencia.getInstancia().getEntityManager();
-        entityManager.getTransaction().begin();
-        Pagamento pagamento=new Pagamento();
-        
-<<<<<<< HEAD
-        String json="{\"codigo\":1459208252077000,\"quando\":1459208252077,\"valor\":0,\"caixa\":{\"codigo\":1459208252076000,\"nome\":\"Caixa central\"}}";
-                
-        ObjectMapper mapper = new ObjectMapper();
-        Saldo saldo=mapper.readValue(json, Saldo.class);
-        
-        System.out.println("--->"+saldo.getCaixa().getNome());
-        
-<<<<<<< HEAD
-        SuperEntidadeService<Caixa> caixaService = new SuperEntidadeService<>(Caixa.class);
-        EntityManager entityManager = Persistencia.getInstancia().getEntityManager();
-        entityManager.getTransaction().begin();
-        Caixa caixa = new Caixa();
-        caixa.setNome("Novo caixa");
-        System.out.println("------------>>>>>>>>>>>> "
-                + caixaService.salvar(caixa));
-        entityManager.getTransaction().commit();
-           entityManager.close();
-        Persistencia.getInstancia().encerra();
-        
-//        SuperEntidadeService<MovimentoCaixa> movimentoCaixaService = new SuperEntidadeService<>(MovimentoCaixa.class);
-//        EntityManager entityManager2 = Persistencia.getInstancia().getEntityManager();
-=======
-
-=======
-
-        
->>>>>>> 3d1f1b6ab087d1ebd6337b45af6813333007803e
-
-//        SuperEntidadeService<Caixa> caixaService = new SuperEntidadeService<>(Caixa.class);
-//        EntityManager entityManager = Persistencia.getInstancia().getEntityManager();
->>>>>>> 358b6331c011dc952dae0c6b2d18592ccb4b571e
-//        entityManager.getTransaction().begin();
-//        MovimentoCaixa movimentoCaixa = new MovimentoCaixa();
-//        movimentoCaixa.setCaixa(caixa);
-//        movimentoCaixa.setHistorico("Histórico");
-//        movimentoCaixa.setRealizado(true);
-//        movimentoCaixa.setValor(new BigDecimal("1000"));
-//        System.out.println("------------>>>>>>>>>>>> "
-//                + movimentoCaixaService.salvar(movimentoCaixa));
-//        entityManager2.getTransaction().commit();
-//           entityManager2.close();
-//        Persistencia.getInstancia().encerra();        
         
     }
 
--- src/main/java/br/com/munif/financeiro/api/CategoriaApi.java
+++ src/main/java/br/com/munif/financeiro/api/CategoriaApi.java
@@ -1,23 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.com.munif.financeiro.api;
 
 import br.com.munif.financeiro.entidades.Categoria;
 import javax.servlet.annotation.WebServlet;
 
-/**
- *
- * @author fatecie26
- */
-<<<<<<< HEAD
 @WebServlet(name = "CategoriaApi", urlPatterns = {"/api/categoria/*"})
-=======
-@WebServlet(name = "CategoriaApi", urlPatterns = {"/api/categoria"})
->>>>>>> 0d0e7c3eaacca1925ae74e99fae9139aa97b4236
 public class CategoriaApi extends SuperEntidadeApi<Categoria>{
 
     @Override
